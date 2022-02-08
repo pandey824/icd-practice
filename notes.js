a5ada6af7633c5fbd6e7ab66cf71f15c93381b5e
AyushiPandey <gayushipandey@gmail.com>
2022-02-08 13:48:46 +0530

    First commit 2022

---
+++ notes.js
@@ -0,0 +1,6 @@
+console.log("Welcome to notes app.This is app.js");
+
+let addBtn = document.getElementById('addBtn');
+addBtn.addEventListener("click",function(e){
+    let addtxt = getElementById
+})
---
+++ practice.js
@@ -0,0 +1,40 @@
+
+
+var controller = new ScrollMagic.Controller();
+ var scene = new ScrollMagic.Scene(
+     {triggerElement :"#trigger1" 
+ })
+  .setTween("#animate1",0.5,{bacgroundColor: "green ",scale :2.5})
+  .addIndicators({name : "1(duration:0"})
+  .addTo(controller);
+
+// creating the scroll bar fucntion
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+});
